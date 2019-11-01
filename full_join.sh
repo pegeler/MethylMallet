5ac245d43f41b5e6f3f32dc23e55472857093074
Paul W. Egeler, M.S., GStat <32426645+pegeler@users.noreply.github.com>
2019-10-31 21:41:09 -0400

    cleaning up parallel code

--- full_join.sh
+++ full_join.sh
@@ -1,49 +1,92 @@
 #!/bin/bash
 
+progname=$(basename $0)
+
 set -e
 
-mkdir -p working
+# Usage -----------------------------------------------------------------------
+function usage {
+  echo "Usage: $progname [-d dir] [-S buffer_size] [-o out_file] [-p] FILE [FILE ...]" >&2
+  exit 1
+}
+
+# Options ---------------------------------------------------------------------
+while getopts ":d:S:o:p" opt; do
+  case $opt in
+    d)
+      work_dir=$OPTARG
+      ;;
+    S)
+      buffer_size=$OPTARG
+      ;;
+    o)
+      out_file=$OPTARG
+      ;;
+    p)
+      do_par=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+if [[ -z "$buffer_size" ]]; then
+  buffer_size=1024b
+fi
+
+mkdir -p "$work_dir"
 
 # SORT ------------------------------------------------------------------------
-echo "$(basename $0): Sorting the inputs..." >&2
+echo "$progname: Sorting the inputs..." >&2
 
 # Sort all of our input data files
-if [ -x "$(command -v parallel)" ]; then
-  echo "$(basename $0): Sorting files in parallel" >&2
+if [[ -n "$do_par" && -x "$(command -v parallel)" ]]; then
+  echo "$progname: Sorting files in parallel" >&2
   parallel --eta --noswap --load 80% \
-    'tail -q -n +2 {} | sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -S 1G -o "working/sorted_{/}"' \
+    $(printf 'tail -q -n +2 {} | sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -S %s -o "%s/sorted_{/}"' $buffer_size $work_dir) \
     ::: "$@"
 else
-  echo "$(basename $0): Sorting files one-by-one" >&2
+  echo "$progname: Sorting files one-by-one" >&2
   for f in "$@"; do
-    echo "$(basename $0):   Working on $(basename $f .tsv)" >&2
+    echo "$progname:   Working on $(basename $f .tsv)" >&2
     tail -q -n +2 "$f" | \
-      sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -S 2G -o "working/sorted_$(basename $f)"
+      sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -S $buffer_size -o "${work_dir}/sorted_$(basename $f)"
   done
 fi
 
 # KEY FILE --------------------------------------------------------------------
-echo "$(basename $0): Making the key file..." >&2
+echo "$progname: Making the key file..." >&2
 
 # Put down the header
-echo "chrom,pos,strand,mc_class" > working/out.csv
+echo "chrom,pos,strand,mc_class" > ${work_dir}/out.csv
 
 # Find the keys and write to csv
-sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -u -m -S 2G working/sorted_*.tsv | \
+sort -k 1n,1 -k 2n,2 -k 3,3 -k 4,4 -u -m -S $buffer_size ${work_dir}/sorted_*.tsv | \
   cut -f 1,2,3,4 | \
   tr '\t' , \
-  >> working/out.csv
+  >> ${work_dir}/out.csv
 
 # APPEND ----------------------------------------------------------------------
-echo "$(basename $0): Appending columns..." >&2
+echo "$progname: Appending columns..." >&2
 
 # Append columns one-by-one using python3 script
-for f in working/sorted_*.tsv; do
-  echo "$(basename $0):   Working on $(basename $f .tsv)" >&2
-  cut -f 1,2,3,4,7 $f | python3 src/do_join.py "$(basename $f .tsv)"
+for f in ${work_dir}/sorted_*.tsv; do
+  echo "$progname:   Working on $(basename $f .tsv)" >&2
+  python3 src/do_join.py "$f"
 done
 
 # DONE ------------------------------------------------------------------------
-rm working/sorted_*.tsv
-echo "$(basename $0): Success! All files joined." >&2
-echo "$(basename $0): Combined comma-separated file saved in 'working/out.csv'" >&2
+rm ${work_dir}/sorted_*.tsv
+
+mv "${work_dir}/out.csv" "$out_file"
+
+echo "$progname: Success! All files joined." >&2
+echo "$progname: Combined comma-separated file saved in '$out_file'" >&2
--- src/do_join.py
+++ src/do_join.py
@@ -19,34 +19,38 @@ Created on Mon Oct 28 19:08:07 2019
 import sys
 import re
 import shutil
+import os.path
 
-temp = open('working/tmp.csv', 'w+')
-work = open('working/out.csv', 'r')
+# Get inputs
+input_file = sys.argv[1]
+work_dir, file_name = os.path.split(input_file)
 
-# Header ---
-header = work.readline().strip()
-header += ',' + re.search(r'^sorted_(.*?)_', sys.argv[1])[1] + '\n'
+# Open files
+temp = open(os.path.join(work_dir, 'tmp.csv'), 'w+')
+work = open(os.path.join(work_dir, 'out.csv'), 'r')
+infile = open(input_file, 'r')
+
+# Header
+header = work.readline().strip() + ','
+header += re.search(r'^sorted_(.*?)_', file_name)[1] + '\n'
 temp.write(header)
 
-del header
-
 # Preload first line from stdin
-candidate = sys.stdin.readline().strip().split()
+candidate = infile.readline().strip().split()
 
 # Loop over working file
 for line in work:
-    line = line.strip()
+    temp.write(line.strip() + ',')
     if line.split(',', 4)[:4] == candidate[:4]:
-        # Write line!
-        temp.write(line + ',' + candidate[-1] + '\n')
-        # Load in next line
-        candidate = sys.stdin.readline().strip().split()
-    else:
-        temp.write(line + ',\n')
+        temp.write(candidate[-1])
+        candidate = infile.readline().strip().split()
+    temp.write('\n')
 
 # Close file connections
 temp.close()
 work.close()
+infile.close()
 
 # At last, copy tmp into out
-shutil.move('working/tmp.csv', 'working/out.csv')
+shutil.move(os.path.join(work_dir, 'tmp.csv'),
+            os.path.join(work_dir, 'out.csv'))
