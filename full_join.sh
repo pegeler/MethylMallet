3e3efb406f7c19d29c4be3fd2b3a855f6753e6bb
pegeler <32426645+pegeler@users.noreply.github.com>
2019-11-05 15:28:08 -0500

    Update full_join.sh

--- full_join.sh
+++ full_join.sh
@@ -20,7 +20,7 @@ required arguments:
   -o OUT_FILE     file name to be output to
 
 optional arguments:
-  -h,             show this help message and exit
+  -h              show this help message and exit
   -p              do sorting operations using GNU parallel
   -S BUFFER_SIZE  buffer size allocated to sorting operation
 EOF
