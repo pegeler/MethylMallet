9f3b75b6d70af61af8b37c915289a83c436d909a
pegeler <32426645+pegeler@users.noreply.github.com>
2019-12-01 01:24:16 -0500

    fixing check_output.r to reflect data name change

--- test/check_output.r
+++ test/check_output.r
@@ -3,7 +3,7 @@
 ddir <- "data/"
 
 files <- list.files(ddir, full.names = TRUE)
-file_names <- strcapture("(test[0-9])_", files, proto = list(character(1)))[,1]
+file_names <- strcapture("(GSM[0-9])_", files, proto = list(character(1)))[,1]
 
 dat <- lapply(
   seq_along(files),
@@ -20,7 +20,7 @@ head(dat)
 
 library(dplyr)
 
-ref <- 
+ref <-
   Reduce(
     function(x,y) full_join(x, y, by = c("chrom", "pos", "strand", "mc_class")),
     dat
@@ -31,6 +31,13 @@ ref <-
 
 # Checking against processed data -----------------------------------------
 
-check <- read.csv('out/out.csv', stringsAsFactors = FALSE)
+check <- read.delim('out/new.tsv.gz', stringsAsFactors = FALSE)
+check %<>%
+  select(
+    c("chrom", "pos", "strand", "mc_class",
+      "GSM1", "GSM2", "GSM3", "GSM4", "GSM5")
+  ) %>%
+  arrange(chrom, pos, strand, mc_class) %>%
+  as.data.frame
 
 all.equal(check, ref)
