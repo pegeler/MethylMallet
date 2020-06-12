2a27f08be20ca699a50682f0dfefc6360cd0bc07
pegeler <32426645+pegeler@users.noreply.github.com>
2020-06-12 01:36:23 -0400

    adding quick little test script

---
+++ test/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+REFERENCE=2de1b2050d4223ff99eb39d23d4b7944
+
+mkdir -p out
+
+echo "Testing parallel..." >&2
+
+../full_join.sh -d work -j 2 -n 5 -S10M -o out/test.csv.gz data/GSM*
+
+CHECKSUM=$(zcat out/test.csv.gz | md5sum | cut -d" " -f1)
+
+if [[  "$REFERENCE" == "$CHECKSUM" ]]; then
+  echo Parallel OK >&2
+else
+  echo Parallel NOT OK >&2
+fi
+
+echo "Testing single..." >&2
+
+../full_join.sh -d work -n 5 -S10M -o out/test.csv.gz data/GSM*
+
+CHECKSUM=$(zcat out/test.csv.gz | md5sum | cut -d" " -f1)
+
+if [[  "$REFERENCE" == "$CHECKSUM" ]]; then
+  echo Single OK >&2
+else
+  echo Single NOT OK >&2
+fi
+
+rm out/test.csv.gz
